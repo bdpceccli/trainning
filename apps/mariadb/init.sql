8d5df2fddfebc32d7ea5a04be537c448370c67a4
bdpceccli <bdp_ceccli@gmx.fr>
2021-06-26 18:26:55 +0200

    conteneur mariadb OK

---
+++ apps/mariadb/init.sql
@@ -0,0 +1,9 @@
+
+databaseproducts_ coord(
+   coord_id INT NOT NULL AUTO_INCREMENT,
+   coord_lat DECIMAL SIGNED,
+   coord_long DECIMAL SIGNED,
+   submission_date DATE,
+   PRIMARY KEY ( coord_id )
+);
+
